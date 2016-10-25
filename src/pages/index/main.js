f878793dccdd7431330464de714fc1c127ee6f38
blade254353074 <blade254353074@hotmail.com>
2016-10-25 15:41:29 +0800

    Add eslintrc

--- src/pages/index/main.js
+++ src/pages/index/main.js
@@ -1,13 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-
-System.import('./App').then(App => {
-  /* eslint-disable no-new */
-  new Vue({
-    el: '#app',
-    render: h => h(App.default)
+Promise.all([
+  System.import('vue'),
+  System.import('./App')
+])
+  .then(([Vue, App]) => {
+    /* eslint-disable no-new */
+    new Vue({
+      el: '#app',
+      render: h => h(App.default)
+    })
   })
-}).catch(err => {
-  console.error(err)
-})
+  .catch(err => {
+    console.error(err)
+  })
